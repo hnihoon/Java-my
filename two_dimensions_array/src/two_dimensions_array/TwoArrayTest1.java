b93f20ce60d24797b15e7bb07e4d80704e08f460
hnihoon <kdh462601@gmail.com>
2023-08-07 00:53:45 +0900

    평일 Java공부

---
+++ two_dimensions_array/src/two_dimensions_array/TwoArrayTest1.java
@@ -0,0 +1,45 @@
+package two_dimensions_array;
+
+public class TwoArrayTest1 {
+
+	public static void main(String[] args) {
+
+//		성적표에 대한 2차 배열 생성
+		int[][] grade = new int[3][3];
+		
+//		kim의 과목별 점수
+		grade[0][0] = 75;
+		grade[0][1] = 82;
+		grade[0][2] = 91;
+		
+//		park의 과목별 점수
+		grade[1][0] = 88;
+		grade[1][1] = 64;
+		grade[1][2] = 50;
+		
+//		lee의 과목별 점수
+		grade[2][0] = 100;
+		grade[2][1] = 100;
+		grade[2][2] = 90;
+		
+//		2차 배열을 탐색하여 총점과 평균점수 구하기
+		for(int i = 0; i<grade.length; i++) {  //행 시작
+//			반복문 안에서 선언된 변수는 매 실행시마다 새로 생성된다.
+			int sum = 0;
+			int avg = 0;
+			
+			for(int j = 0; j<grade[i].length; j++) {  //열 시작
+//				i번째 위치의 j번째에 각 과목별 점수에 대한 누적처리
+				sum += grade[i][j];
+			} //열 끝
+			
+//			평균
+			avg = sum / grade[i].length;
+			
+			System.out.println("총점 : " + sum + ", 평균 : " + avg);
+			
+		} //행 끝
+		
+	}
+
+}
